23b76bc0a8c9e7287fa54cc56f1dcd3579d2a218
djmaxus <44438314+djmaxus@users.noreply.github.com>
2025-04-10 16:05:34 +0100

    test: ✅ add test
    Release-As: 0.1.0
    
    Basic functionality test in a thread-based pool.

--- test.m
+++ test.m
@@ -1,1 +1,11 @@
-assert(true);
+if isempty(gcp('nocreate'))
+    parpool('Threads',2);
+end
+num_iterations = 8;
+[self, update_queue] = ParWaitBar(num_iterations,'Name','Test','visible','off');
+parfor (i=1:num_iterations,2)
+    pause(1);
+    send(update_queue,[]);
+end
+self.finish();
+assert(self.state==num_iterations);
